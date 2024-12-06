e1bef0108e197b034b10297e1b5b68124536c3ce
Roman <39233213+RomanEmreis@users.noreply.github.com>
2024-12-06 23:07:50 +0400

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -29,7 +29,10 @@ export default defineUserConfig({
       },
     ],
     sidebar: [
-      'Home',
+      {
+        text: 'Home',
+        link: '/',
+      },
       {
         text: 'Getting Started',
         prefix: '/getting-started/',
