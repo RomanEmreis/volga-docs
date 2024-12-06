42ed68c2dc0e780a40530d1c8a3d9a973d8d579a
Roman Emreis <Roman_Emreis@epam.com>
2024-12-06 20:27:12 +0400

    cleanup

--- docs/.vuepress/.temp/pages/timeline.html.js
+++
@@ -1,3 +0,0 @@
-import comp from "C:/Source/Repos/volga/volga-docs-v2/vuepress-starter/docs/.vuepress/.temp/pages/timeline.html.vue"
-const data = JSON.parse("{\"path\":\"/timeline.html\",\"title\":\"\",\"lang\":\"en-US\",\"frontmatter\":{\"layout\":\"Timeline\"},\"headers\":[],\"git\":{},\"filePathRelative\":null}")
-export { comp, data }
